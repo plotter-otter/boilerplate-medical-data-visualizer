88d6929c41669a6fb30baac568b717d9d4946a58
Yanitsa Aleksandrova <yanitsa.al@gmail.com>
2024-08-21 16:47:43 +0000

    modified:   medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,13 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv', delimiter = ',').set_index('id')
 
 # 2
-df['overweight'] = None
+df['overweight'] = df['weight']/((df['height']/100)**2)
+df['overweight'] = (df['overweight'] > 25).astype(int)
 
 # 3
+#simpler
+df['cholesterol'] = df['cholesterol'].replace({1: 0, 2: 1, 3: 1})
+df['gluc'] = df['gluc'].replace({1: 0, 2: 1, 3: 1})
 
+#more generalised method, focus on the > 1 condition, doesn't require us to know all possible values
+#df[['cholesterol','gluc']]=df[['cholesterol','gluc']].apply(lambda col: np.where(col > 1, 1 ,0))
+
+#data cleaning
+df = df[
+    (df['ap_lo'] <= df['ap_hi']) &
+    (df['height'] >= df['height'].quantile(0.025)) &
+    (df['height'] <= df['height'].quantile(0.975)) &
+    (df['weight'] >= df['weight'].quantile(0.025)) &
+    (df['weight'] <= df['weight'].quantile(0.975))
+]
 
 # 4
 def draw_cat_plot():
@@ -27,7 +42,9 @@ def draw_cat_plot():
 
 
     # 8
-    fig = None
+    fig = sns.catplot(
+    data=df, x="class", y="survived", col="cardio",
+    kind="bar")
 
 
     # 9
